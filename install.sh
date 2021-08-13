4b89ca03753e5af9b08f4f319531a47c3d6ab71b
Andrew Johnston <apjohnsto@gmail.com>
2021-08-13 16:22:02 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 JUST_VERSION=0.1.2
 curl -L -o just.tar.gz https://github.com/just-js/just/archive/${JUST_VERSION}.tar.gz
 tar -zxvf just.tar.gz
-mv just-${JUST_VERSION} .just
+mv just-${JUST_VERSION} just
 make -C just runtime-static
