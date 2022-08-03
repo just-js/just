e1a49abdd662e176f4d579eb82ac2054a9721ce8
Andrew Johnston <apjohnsto@gmail.com>
2022-08-03 08:43:03 +0100

    bump version

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-JUST_VERSION=0.1.9
+JUST_VERSION=0.1.10
 curl -L -o just.tar.gz https://github.com/just-js/just/archive/${JUST_VERSION}.tar.gz
 tar -zxvf just.tar.gz
 mv just-${JUST_VERSION} just
